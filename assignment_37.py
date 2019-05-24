e62a49c2d5de620b20ab137e552430b632dc119a
Angayarkkanni9 <50651624+Angayarkkanni9@users.noreply.github.com>
2019-05-24 09:47:34 +0530

    Update and rename assignment_37 to assignment_37.py

--- assignment_37.py
+++ assignment_37.py
@@ -23,16 +23,5 @@ def reward_child(child_id_rewarded,extra_chocolates):
             print(chocolates_received)
         else:
             print("Child id is invalid")
-
-
-    # Use the below given print statements to display the output
-    # Also, do not modify them for verification to work
-
-    #print("Extra chocolates is less than 1")
-    #print("Child id is invalid")
-    #print(chocolates_received)
-
-
 print(calculate_total_chocolates())
-#Test your code by passing different values for child_id_rewarded,extra_chocolates
 reward_child(20,2)
