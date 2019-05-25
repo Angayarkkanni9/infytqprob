0fd94b3552dc0312a5d24f4200bf31a31f1348ff
Angayarkkanni9 <50651624+Angayarkkanni9@users.noreply.github.com>
2019-05-25 15:20:54 +0530

    Create practice_10.py

---
+++ practice_10.py
@@ -0,0 +1,11 @@
+def string_both_ends(input_string):
+	if(len(input_string)<2):
+	    return -1
+	else:
+	    s=""
+	    s=input_string[0:2]+input_string[len(input_string)-2:len(input_string)]
+	    return s
+
+input_string="w3"
+print(string_both_ends(input_string))
+©
